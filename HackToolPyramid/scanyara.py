0a923adda6155b6c91f3f6157ecaf7f0641f08cb
HydraDragonAntivirus <130374789+Siradankullanici@users.noreply.github.com>
2025-01-14 22:05:13 +0300

    Payload path

--- HackToolPyramid/scanyara.py
+++ HackToolPyramid/scanyara.py
@@ -4,14 +4,25 @@ import os
 # Get current working directory
 script_dir = os.getcwd()
 
-# Path to Yara rule file and payloads file
 rule_file = os.path.join(script_dir, 'yararule', 'HackToolPythonPyramid.yar')
 payloads_file = os.path.join(script_dir, 'payloads', 'onemorestepleft.py')
+# Hardcoded path for the Yara rule file
+rule_file = os.path.join(script_dir, 'yararules', 'HackToolPythonPyramid.yar')
+
+# Prompt the user to input the payload file path
+payloads_file_input = input(f"Enter the path to the payload file (relative to {script_dir}): ")
+
+# Combine user input with the current working directory for payload file
+payloads_file = os.path.join(script_dir, payloads_file_input)
 
 # Load Yara rule
-rules = yara.compile(filepath=rule_file)
+try:
+    rules = yara.compile(filepath=rule_file)
+except yara.YaraSyntaxError as e:
+    print(f"Error compiling Yara rule: {e}")
+    exit(1)
 
-# Check if the file exists
+# Check if the payload file exists
 if os.path.exists(payloads_file):
     print(f"Scanning started for: {payloads_file}")
     
